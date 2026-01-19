83ab8bb23b7b29452125868efc1ae52f6a9c66fd
Hitiksha Jasani <144584512+Hitiksha7@users.noreply.github.com>
2026-01-19 18:54:54 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -31,23 +31,3 @@ class EmbeddingWrapper:
 embeddings = EmbeddingWrapper()
  
 
-# #embeddings.py
-# from langchain_openai import OpenAIEmbeddings
-# from langchain_community.vectorstores import Qdrant
-# from qdrant_client import QdrantClient
-# from dotenv import load_dotenv
-# import os
-
-# # Load environment variables
-# load_dotenv()
-
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-# QDRANT_URL = os.getenv("QDRANT_URL")
-# QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
-# HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
-
-# # Initialize Qdrant client and embeddings
-# qdrant_client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY, timeout=60)
-# embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY, model="text-embedding-ada-002")
-# VECTOR_SIZE = 1536
-# DISTANCE_METRIC = "Cosine"
